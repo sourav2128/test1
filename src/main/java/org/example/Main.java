3f8d219dd0d328bd6e7cdab67bea0e2264c508c7
sourav2128 <108931336+sourav2128@users.noreply.github.com>
2025-05-12 12:44:40 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,7 +14,7 @@ public class Main {
         Matcher matcher = pattern.matcher(input);
 
         if (matcher.matches()){
-            System.out.println("V_alid");
+            System.out.println("V__alid");
         }
         else {
             System.out.println("invalid");
